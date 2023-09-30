c33ccccab2bee37914861dca173a6e82ef1f5bc7
lixel_v <simeongfremenko@gmail.com>
2023-10-01 01:19:27 +0300

    main

--- server/main.py
+++ server/main.py
@@ -1,18 +1,23 @@
 from db import SQLite
-import json
+from time import time
 from fastapi import FastAPI, Request, status
+import json
 import logging
 
 logging.basicConfig(level=logging.ERROR, filename='app.log')
 sql = SQLite('db.db')
 app = FastAPI()
+
 app.add_event_handler('startup', sql.init)
+app.add_event_handler('shutdown', sql.close)
 
 with open('main.json', 'r') as file:
     default = json.load(file)
 
 @app.get("/")
 async def read_root(request: Request):
+    tm = default["sleep"]
+
     ip = request.client.host
 
     if await sql.pc_exists(ip):
@@ -20,14 +25,25 @@ async def read_root(request: Request):
 
     content = await sql.api_read(ip)
 
+    result = {"args": content}
+    result.update(default)
+    result["sleep"] = tm - (time() % tm)
+
     if content is None:
-        return default
+        return result
 
     else:
-        result = {"args": content}
-        result.update(default)
         result["run"] = True
+
         return result
+
+@app.get("/sleep")
+async def sleep_timing():
+    tm = default["sleep"]
+    result = default.copy()
+    result["sleep"] = tm - (time() % tm)
+    return result
+
 
 @app.get("/success", status_code=status.HTTP_204_NO_CONTENT)
 async def client_success(request: Request, task: str):
@@ -37,8 +53,3 @@ async def client_success(request: Request, task: str):
 @app.post("/error", status_code=status.HTTP_204_NO_CONTENT)
 async def client_error(request: Request, error: str):
     logging.error(f'ERROR:     {request.client.host} - {error}')
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    if sql is not None:
-        await sql.close()
